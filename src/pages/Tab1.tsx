34ad17e5342141753f290019df0bd9fb3f0a9e6e
fazlynz <31564681+fazlynz@users.noreply.github.com>
2023-01-07 16:28:59 +0800

    Update Tab1.tsx

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,125 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { useState } from "react";
 import './Tab1.css';
+import crypto from "crypto-js";
+//import copyToClipboard from "react";
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
 
-export default Tab1;
+
+function App() {
+    const [aes_hashed_text,setAesHashedText] = useState("");
+    const [md5_hashed_text,setMd5HashedText] = useState("");
+    const [sha512_hashed_text,setSha512HashedText] = useState("");
+    const [sha256_hashed_text,setSha256HashedText] = useState("");
+    const [sha3_hashed_text,setSha3HashedText] = useState("");
+    // user input text
+    const [plain_text, setPlainText] = useState("");
+
+    // submit handler
+    const onSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        // md5
+    setMd5HashedText(
+        
+        crypto.MD5(plain_text).toString()
+        
+    );
+
+    // sha512
+    setSha512HashedText(
+        crypto.SHA512(plain_text).toString()
+    );
+
+    // sha256
+    setSha256HashedText(
+        crypto.SHA256(plain_text).toString()
+    );
+
+    //sha3
+    setSha3HashedText(
+        crypto.SHA3(plain_text).toString()
+    );
+
+    }
+
+    function copyToClipboard(md5_hashed_text: string): void {
+        throw new Error("Function not implemented.");
+    }
+
+    return (
+        <div className="App">
+
+            <form className="form" onSubmit={onSubmit}>
+
+                <div className="form-content">
+                {
+    md5_hashed_text ? (
+        <div className="form-group">
+            <label>md5</label>
+            <div className="hashed-data-content">
+                <input type='text' readOnly value={md5_hashed_text} />
+                <button type="button" onClick={() => copyToClipboard(md5_hashed_text)}>copy</button>
+            </div>
+        </div>
+    ) : null
+}
+
+{
+    sha512_hashed_text ? (
+        <div className="form-group">
+            <label>SHA512</label>
+            <div className="hashed-data-content">
+                <input type='text' readOnly value={sha512_hashed_text} />
+                <button type="button" onClick={() => copyToClipboard(sha512_hashed_text)}>copy</button>
+            </div>
+        </div>
+    ) : null
+}
+
+{
+    sha256_hashed_text ? (
+        <div className="form-group">
+            <label>SHA256</label>
+            <div className="hashed-data-content">
+                <input type='text' readOnly value={sha256_hashed_text} />
+                <button type="button" onClick={() => copyToClipboard(sha256_hashed_text)}>copy</button>
+            </div>
+        </div>
+    ) : null
+}
+
+{
+    sha3_hashed_text ? (
+        <div className="form-group">
+            <label>SHA3</label>
+            <div className="hashed-data-content">
+                <input type='text' readOnly value={sha3_hashed_text} />
+                <button type="button" onClick={() => copyToClipboard(sha3_hashed_text)}>copy</button>
+            </div>
+        </div>
+    ) : null
+}
+                    <div className="form-group">
+                        <label>
+                            Sample text
+                        </label>
+                        <input type="text" placeholder="Enter any text" value={plain_text} onChange={
+                            (e) => setPlainText(e.target.value)
+                        } />
+                    </div>
+
+
+                    <div className="form-group">
+                        <button className="submit-btn" type="submit">
+                            Generate hashed
+                        </button>
+                    </div>
+
+                </div>
+
+            </form>
+
+        </div>
+    );
+}
+export default App;
